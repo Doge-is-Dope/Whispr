74c98c6c911a0c2b754feef688d17e33267afa20
clement.l <maple3312936@outlook.com>
2025-08-31 23:09:08 +0800

    chore: add workaround to fix hilt warning

--- app/src/main/java/com/clementl/whispr/WhisprApp.kt
+++ app/src/main/java/com/clementl/whispr/WhisprApp.kt
@@ -1,11 +1,19 @@
 package com.clementl.whispr
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.qualifiers.ApplicationContext
+import jakarta.inject.Inject
 import timber.log.Timber
 
 @HiltAndroidApp
 class WhisprApp : Application() {
+    // TODO Delete when https://github.com/google/dagger/issues/3601 is resolved.
+    @Inject
+    @ApplicationContext
+    lateinit var context: Context
+
     override fun onCreate() {
         super.onCreate()
 
